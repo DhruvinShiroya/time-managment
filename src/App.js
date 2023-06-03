fc761fc4c9115a288c73355a83f1197a18892e30
Dhruvin Shiroya <shiroyadhruvinm@gmail.com>
2023-06-03 01:51:47 -0400

    signup page is created
    validation for email, userName , password is added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Layout from './container/Layout';
+import Register from './container/Register';
 
 function App() {
   return (
     <div className="App">
       <Layout/>
+      <Register/> 
     </div>
   );
 }
